4063444de1b5c307a41b9e4d63de88b8d0515fb6
lrfung <luorongfang126@126.com>
2016-09-29 16:08:58 +0800

    修改路由

--- src/main.js
+++ src/main.js
@@ -1,19 +1,18 @@
 import Vue from "vue";  
 import VueRouter from "vue-router";  
+import login from './components/login.vue';
+import register from './components/register.vue';
+
 Vue.use(VueRouter);
   
 var App = Vue.extend({})  
 var router = new VueRouter({hashbang: false, history: true})  
 router.map({  
     '/login': {  
-        component: function (resolve) {  
-          require(['./components/login.vue'], resolve)  
-        }  
+        component:login
     },  
     '/register': {  
-        component: function (resolve) {  
-          require(['./components/register.vue'], resolve)  
-        }  
+        component:register
     }  
       
 })  
